12dfe1bd147f1fdebfe171d8c68d8b52edaf66a7
KEERTHI AANAND K S <keerthiaanand77@gmail.com>
2026-09-05 13:19:02 +0530

    feat: initialize project scaffold with Vite, Tailwind CSS, and Prisma schema definitions

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,16 @@
 import react from '@vitejs/plugin-react'
+import path from "path"
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
