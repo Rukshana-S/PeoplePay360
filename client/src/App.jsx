deed2d0c853e0c4e7f10c10e079846ed12c6ee00
KEERTHI AANAND K S <keerthiaanand77@gmail.com>
2026-09-05 11:48:59 +0530

    Frontend Basic Folder and Files

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>PeoplePay360</h1>
+      <p>Integrated HR and Payroll Operations Platform</p>
+      <p>Frontend is running successfully.</p>
+    </main>
+  );
+}
+
+export default App;
